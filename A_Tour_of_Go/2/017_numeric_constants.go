083e7bd6d2857e2b4efef2740b6a3b083f6f56a6
Shipovmax <shipov.max@icloud.com>
2026-04-27 18:13:03 +0300

    Add initial Go files for A Tour of Go examples including functions, variables, and constants

---
+++ A_Tour_of_Go/2/017_numeric_constants.go
@@ -0,0 +1,17 @@
+package atourofgo
+
+import "fmt"
+
+const (
+	Big   = 1 	<< 100
+	Small = Big >> 99
+)
+
+func needInt(x   int)    int    { return x*10 + 1 }
+func needFloat(x float64) float64 { return x * 0.1 }
+
+func numeric_constants() {
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+}
