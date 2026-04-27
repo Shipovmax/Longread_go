c76ab3b4dffe58a78d7a4e9184a3f63943bea569
Shipovmax <shipov.max@icloud.com>
2026-04-27 18:01:45 +0300

    Add type conversion and type inference examples

---
+++ A_Tour_of_Go/014_type_conversions.go
@@ -0,0 +1,13 @@
+package atourofgo
+
+import (
+	"fmt"
+	"math"
+)
+
+func type_conversions() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
---
+++ A_Tour_of_Go/015_type_interence.go
@@ -0,0 +1,8 @@
+package atourofgo
+
+import "fmt"
+
+func type_interence() {
+	v := 42 // change me !!!
+	fmt.Printf("v is of type %T\n", v)
+}
